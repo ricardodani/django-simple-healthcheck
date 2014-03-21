9e74f9d089856a5f8d8dc21eefa72873234c30b3
Ricardo Dani <ricardodani@gmail.com>
2014-03-21 19:09:51 -0300

    Added base urls to /.

--- healthcheck/urls.py
+++ healthcheck/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # Examples:
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^healthcheck/', include('urlchecker.urls')),
+    url(r'^', include('urlchecker.urls')),
 )
